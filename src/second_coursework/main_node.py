5acaf1d3784056802fa54ef8576e1fca856cca33
Sujan Naik <sujannaik047@gmail.com>
2023-12-05 20:59:42 +0000

    Fix service and state implementation

--- src/second_coursework/main_node.py
+++ src/second_coursework/main_node.py
@@ -4,34 +4,23 @@ import rospy
 import std_msgs
 from actionlib_msgs.msg import GoalID
 from move_base_msgs.msg import MoveBaseGoal
+from std_msgs.msg import String
 
 import robot_move_server
 from state_machines import state_machine
 from second_coursework.msg import RoomCheckAction, RoomCheckGoal
-
+from second_coursework.srv import robot_move
 
 try:
     rospy.init_node("time")
 except rospy.exceptions.ROSException as e:
     print("Node has already been initialized")
-goal = MoveBaseGoal()
-goal.target_pose.header.stamp = rospy.get_rostime()
-goal.target_pose.header.frame_id = 'map'
-goal.target_pose.pose.position.x = 1.5
-goal.target_pose.pose.position.y = 8.5
-goal.target_pose.pose.position.z = 0
-goal.target_pose.pose.orientation.x = 0
-goal.target_pose.pose.orientation.y = 0
-goal.target_pose.pose.orientation.z = 0
-goal.target_pose.pose.orientation.w = 1
-robot_move_server.move(goal)
+
+robot_move_server.robot_move_server()
 state_machine.run()
 
-client = actionlib.SimpleActionClient('/server', RoomCheckAction)
+client = actionlib.SimpleActionClient('server', RoomCheckAction)
 client.wait_for_server()
-# header = std_msgs.msg.Header()
-# header.stamp = rospy.Time.now()
-# goalID = GoalID()
 room_check_goal = RoomCheckGoal()
 room_check_goal.times_to_check = 3
 client.send_goal(room_check_goal)
--- src/second_coursework/robot_move_server.py
+++ src/second_coursework/robot_move_server.py
@@ -1,33 +1,65 @@
 #!/usr/bin/env python3
+import actionlib
 import rospy
 import std_msgs
 from actionlib_msgs.msg import GoalID
 from second_coursework.srv import robot_move
-from move_base_msgs.msg import MoveBaseActionGoal
+from move_base_msgs.msg import MoveBaseActionGoal, MoveBaseGoal, MoveBaseAction, MoveBaseActionResult
 
-rospy.init_node('main_node')
-try:
-    rospy.init_node("time")
-except rospy.exceptions.ROSException as e:
-    print("Node has already been initialized")
+'''
+Entrances for Rooms
+A 1.9, 8.3
+B 6, 8.4
+C  10.7, 8.3
+D 2, 3.3
+E 6.1, 3.5
+F 10.6, 2.5
+'''
 
-def move(request):
+
+def robot_move_callback(request):
     """Publishes requests made to move the robot to the move_base goal"""
-    pub = rospy.Publisher(name='/move_base/goal',data_class=MoveBaseActionGoal,queue_size=10)
-    header = std_msgs.msg.Header()
-    header.stamp = rospy.Time.now()
-    goalID = GoalID()
-    pub.publish(header,goalID,request)
 
-def initialiseMovement():
+    goal = MoveBaseGoal()
+    goal.target_pose.header.stamp = rospy.get_rostime()
+    goal.target_pose.header.frame_id = 'map'
+
+    room_name = request.room_name
+
+    if room_name == 'A':
+        goal.target_pose.pose.position.x = 1.9
+        goal.target_pose.pose.position.y = 8.3
+
+    elif room_name == 'B':
+        goal.target_pose.pose.position.x = 6
+        goal.target_pose.pose.position.y = 8.4
+
+    elif room_name == 'D':
+        goal.target_pose.pose.position.x = 2
+        goal.target_pose.pose.position.y = 3.3
+    else:
+        raise ValueError('Must either be A, B or D')
+
+    goal.target_pose.pose.position.z = 0
+    goal.target_pose.pose.orientation.x = 0
+    goal.target_pose.pose.orientation.y = 0
+    goal.target_pose.pose.orientation.z = 0
+    goal.target_pose.pose.orientation.w = 1
+
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    client.wait_for_server()
+    client.send_goal(goal)
+    client.wait_for_result(rospy.Duration.from_sec(20))
+    if client:
+        return True
+
+    return False
+
+
+def robot_move_server():
     """Initialises the movement node and service"""
-    rospy.init_node(name='robot_move_server', anonymous=True)
-    rospy.Service(name='robot_move', service_class=robot_move, handler=move)
-    rospy.spin()
-
-
-if __name__ == '__main__':
     try:
-        initialiseMovement()
-    except rospy.ROSInterruptException:
-        pass
+        rospy.init_node('robot_move_server')
+    except rospy.exceptions.ROSException as e:
+        print("Node has already been initialized")
+    rospy.Service(name='robot_move_service', service_class=robot_move, handler=robot_move_callback)
