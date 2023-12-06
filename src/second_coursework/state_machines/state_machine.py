33a403451f5d8222650be02487bc05507b88e279
Sujan Naik <sujannaik047@gmail.com>
2023-12-06 22:50:52 +0000

    Work on implementing action feedback, need tests

--- src/second_coursework/state_machines/state_machine.py
+++ src/second_coursework/state_machines/state_machine.py
@@ -3,18 +3,26 @@ import math
 
 import actionlib
 import actionlib_msgs.msg
+import geometry_msgs.msg
 import move_base_msgs.msg
 import rospy
 import smach
 import std_msgs
 from actionlib_msgs.msg import GoalID, GoalStatus
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PoseStamped
 from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction
 from smach import StateMachine, Iterator, Sequence, CBState, Concurrence
 from smach_ros import SimpleActionState, ActionServerWrapper
-from second_coursework.msg import RoomCheckAction, RoomCheckActionGoal
+from second_coursework.msg import RoomCheckAction, RoomCheckActionGoal, RoomCheckActionResult, RoomCheckActionFeedback
 from second_coursework.srv import robot_move, yolo_detect
 
+
+def publish_feedback(self, rule_broke):
+    message = rospy.wait_for_message(topic='move_base/feedback', topic_type=PoseStamped)
+    self.feedback.feedback.robot_position = message.pose.position
+    self.feedback.feedback.rule_broken = rule_broke
+    ActionServerWrapper.publish_feedback(self=self,userdata=self)
+    rospy.loginfo('this has happened!!')
 
 
 # Rule 1: No people should be in the kitchen (Room D) at any time
@@ -28,24 +36,25 @@ def robot_move_CB(self, room_name, navigation_time):
             reached_time = rospy.get_rostime().to_time()
 
             rule_1_broken = False
-            rule_2_broken = False
-            rospy.wait_for_service(service='detect_frame')
-            detection_service = rospy.ServiceProxy(name='detect_frame', service_class=yolo_detect)
+            rospy.wait_for_service(service='/detect_frame')
+            detection_service = rospy.ServiceProxy(name='/detect_frame', service_class=yolo_detect)
             while rospy.get_rostime().to_time() - reached_time < navigation_time:
                 cat_detected = False
                 dog_detected = False
                 for detection in detection_service().detections:
-                    if room_name == 'D' and detection.name == 'Person':
+                    if not rule_1_broken and room_name == 'D' and detection.name == 'person':
                         rule_1_broken = True
-                        rospy.loginfo('RULE 1 BROKEN !!!!!')
-                    elif detection.name == 'Cat':
+                        self.result.result.rule_break_amount[0] = self.result.result.rule_break_amount[0]+1
+                        publish_feedback(self,1)
+
+                    if detection.name == 'cat':
                         cat_detected = True
-                    elif detection.name == 'Dog':
+                    if detection.name == 'dog':
                         dog_detected = True
 
                 if dog_detected and cat_detected:
-                    rule_2_broken = True
-                    rospy.loginfo('RULE 2 BROKEN !!!!!')
+                    self.result.result.rule_break_amount[1] = self.result.result.rule_break_amount[1]+1
+                    publish_feedback(self, 2)
 
                 pub = rospy.Publisher(name='/cmd_vel', data_class=Twist, queue_size=10, latch=True)
                 velocity = Twist()
@@ -61,23 +70,40 @@ def robot_move_CB(self, room_name, navigation_time):
 def run():
     sm = StateMachine(outcomes=['succeeded', 'aborted', 'preempted'], input_keys=['goal'], output_keys=['result'])
 
+
     with sm:
+
         it = Iterator(outcomes=['succeeded', 'preempted', 'aborted'],
                       it=lambda: range(0, sm.userdata.goal.times_to_check),
                       input_keys=[],
-                      output_keys=[], exhausted_outcome='succeeded')
+                      output_keys=['result'], exhausted_outcome='succeeded')
 
         with it:
-            container_sm = StateMachine(outcomes=['succeeded', 'aborted', 'preempted', 'continue'])
+            container_sm = StateMachine(outcomes=['succeeded', 'aborted', 'preempted', 'continue'], output_keys=['result'])
+            container_sm.userdata.feedback = RoomCheckActionFeedback()
+            container_sm.userdata.result = RoomCheckActionResult()
+            container_sm.userdata.result.result.rule_break_amount = [0,0]
             with container_sm:
-                container_sm.add(label='room_check_A', state=CBState(cb=robot_move_CB, cb_args=['A', 30], outcomes=['succeeded', 'preempted', 'aborted']),
-                                 transitions={'succeeded': 'room_check_B'})
+                container_sm.add(label='room_check_A',
+                                 state=CBState(cb=robot_move_CB,
+                                               cb_args=['A', 30],
+                                               outcomes=['succeeded', 'preempted', 'aborted'],
+                                               io_keys=['result']),
+                                 transitions={'succeeded': 'room_check_B'}),
 
-                container_sm.add(label='room_check_B', state=CBState(cb=robot_move_CB, cb_args=['B', 30], outcomes=['succeeded', 'preempted', 'aborted']),
+
+                container_sm.add(label='room_check_B',
+                                 state=CBState(cb=robot_move_CB,
+                                               cb_args=['B', 30],
+                                               outcomes=['succeeded', 'preempted', 'aborted'],
+                                               io_keys=['result']),
                                  transitions={'succeeded': 'room_check_D'})
 
-                container_sm.add(label='room_check_D', state=CBState(cb=robot_move_CB, cb_args=['D', 10],
-                                                                     outcomes=['succeeded', 'preempted', 'aborted']),
+                container_sm.add(label='room_check_D',
+                                 state=CBState(cb=robot_move_CB,
+                                               cb_args=['D', 10],
+                                               outcomes=['succeeded', 'preempted', 'aborted'],
+                                               io_keys=['result']),
                                  transitions={'succeeded': 'continue'})
 
 
@@ -92,6 +118,7 @@ def run():
         aborted_outcomes=['aborted'],
         preempted_outcomes=['preempted'],
         goal_key='goal',
+        feedback_key='feedback',
         result_key='result'
     )
     asw.run_server()
